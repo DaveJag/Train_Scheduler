d67f188fe7275f7982138101d5f0605e60b7ee46
David Jagodowski <davejag@comcast.net>
2017-07-24 13:59:23 -0400

    Built interface. Added heading div, table div, form div and styled it with CSS. No functionality.

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -1,1 +1,17 @@
 /* Dave Jagodowski - HW07 - Train Scheduler */
+
+$(document).ready(function(){
+
+    //Write content to title div
+    $("#title").html(
+    	"<h1 class='titleClass'>Anytime is Train Time</h1><h2 class='subtitleClass'>Choo Choo. Chee Chee.</h2>"
+    );
+
+
+   
+
+
+
+
+
+}); //end document ready
